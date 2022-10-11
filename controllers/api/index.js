1eaea05dcdfb0a94ab6eeba26b29ad2750323c26
Chi Chiu Lam <lamcc90@gmail.com>
2022-10-11 03:13:37 -0400

    more changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
+// const userRoutes = require('./user-routes');
 // const postRoutes = require('./post-routes')
 
-router.use('/user', userRoutes);
+// router.use('/user', userRoutes);
 // router.use('/posts', postRoutes)
 
 module.exports = router;
